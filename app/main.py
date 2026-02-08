fc320cf7e802e71af49cec71f62ae4be8788ea2f
Artem Turchyn <Artem.at131@gmail.com>
2026-02-08 17:37:48 +0200

    update

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance) -> Distance:
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, (int, float)):
             return Distance(self.km + other)
 
-    def __iadd__(self, other: Distance) -> Distance:
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
@@ -22,10 +22,10 @@ class Distance:
             self.km += other
             return self
 
-    def __mul__(self, other: Distance) -> float:
+    def __mul__(self, other: int | float) -> Distance:
         return Distance(self.km * other)
 
-    def __truediv__(self, other: Distance) -> float:
+    def __truediv__(self, other: int | float) -> Distance:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance) -> bool:
